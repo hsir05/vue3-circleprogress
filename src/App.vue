7532296a96479960923a7d802f8cb6c50fc1c515
haqsir@163.com <haqsir@163.com>
2022-06-15 21:59:01 +0800

    vue3 实现一个圆环进度条

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="">
+    <CircleProgress :progress="progress" color="#fdd835"  />
+  </div>
+</template>
+<script setup>
+import { ref } from "vue"
+import CircleProgress from "./components/circleProgress.vue";
+
+const progress = ref(50)
+setInterval(() => {
+    progress.value = Math.floor(Math.random()*100+1)
+}, 1000);
+</script>
+
